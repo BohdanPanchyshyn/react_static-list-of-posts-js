9b5826bad5f7e5512fab688bf8398def7b584135
SageUA <panchyshyn33@gmail.com>
2026-01-09 12:40:58 +0200

    for ask 2 errors

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ post }) => {
+
+  return (
+    <div className="CommentList">
+
+      {post.comments.map(comment => (
+         <CommentInfo key={comment.id} comment={comment} />
+       ))}
+
+    </div>
+  );
+};
